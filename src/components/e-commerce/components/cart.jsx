e1f5bfd34b6b447b47f6e23ce65bb83d26079519
Dlassoc <99360471+Dlassoc@users.noreply.github.com>
2023-11-10 14:32:00 -0500

    Update cart.jsx
    Comments and simplify cards logic

--- src/components/e-commerce/components/cart.jsx
+++ src/components/e-commerce/components/cart.jsx
@@ -1,69 +1,82 @@
 import { useUserContext } from "../../../context";
 
-// eslint-disable-next-line react/prop-types
 const Cart = ({ products, handleVisibility }) => {
+  // Obtén el contexto del usuario para acceder y actualizar los datos del carrito
   const { userData, setUserData } = useUserContext();
 
-  function handleAdd(id) {
+  // Maneja la acción de agregar un producto al carrito
+  const handleAdd = (id) => {
     setUserData((lastValue) => {
-      const value = lastValue.cart[id];
-      if (value.unitsTaken == value.units) return lastValue;
-      value.unitsTaken += 1;
-      return { ...lastValue, cart: { ...lastValue.cart, [value.name]: value } };
+      const cartItem = lastValue.cart[id];
+
+      // Verifica si la cantidad tomada ya alcanzó el límite disponible
+      if (cartItem.unitsTaken === cartItem.units) {
+        return lastValue; // No se permite agregar más unidades
+      }
+
+      // Incrementa la cantidad tomada y actualiza el carrito
+      cartItem.unitsTaken += 1;
+      return { ...lastValue, cart: { ...lastValue.cart, [id]: cartItem } };
     });
-  }
-  function handleMinus(id) {
+  };
+
+  // Maneja la acción de reducir la cantidad de un producto en el carrito
+  const handleMinus = (id) => {
     setUserData((lastValue) => {
-      const value = lastValue.cart[id];
-      if (value.unitsTaken == 1) {
-        // eslint-disable-next-line no-unused-vars
+      const cartItem = lastValue.cart[id];
+
+      // Verifica si la cantidad tomada es 1, en cuyo caso se elimina el producto del carrito
+      if (cartItem.unitsTaken === 1) {
         const { [id]: itemOut, ...newCart } = lastValue.cart;
         return { ...lastValue, cart: newCart };
       }
-      value.unitsTaken -= 1;
-      return { ...lastValue, cart: { ...lastValue.cart, [value.name]: value } };
+
+      // Reduz la cantidad tomada y actualiza el carrito
+      cartItem.unitsTaken -= 1;
+      return { ...lastValue, cart: { ...lastValue.cart, [id]: cartItem } };
     });
-  }
+  };
 
   return (
     <aside className="shopping-cart">
+      {/* Botón para cerrar el carrito */}
       <input
         className="close"
         type="button"
         value="x"
         onClick={handleVisibility}
       />
+      {/* Lista de productos en el carrito */}
       <article>
-        {Object.values(products).map(({ name, price, unitsTaken, image }) => {
-          return (
-            <section key={crypto.randomUUID()}>
-              <img src={image} alt="" />
-              <div>
-                <p>{name}</p>
-                <p>
-                  {(price * unitsTaken).toLocaleString("en", {
-                    style: "currency",
-                    currency: "USD",
-                  })}
-                </p>
-                <p>Cantidad: {unitsTaken}</p>
-              </div>
-              <div className="right">
-                <button onClick={() => handleAdd(name)}>+</button>
-                <button onClick={() => handleMinus(name)}>-</button>
-              </div>
-            </section>
-          );
-        })}
+        {Object.values(products).map(({ name, price, unitsTaken, image }) => (
+          <section key={crypto.randomUUID()}>
+            <img src={image} alt="" />
+            <div>
+              <p>{name}</p>
+              <p>
+                {/* Muestra el precio total del producto en función de la cantidad tomada */}
+                {(price * unitsTaken).toLocaleString("en", {
+                  style: "currency",
+                  currency: "USD",
+                })}
+              </p>
+              <p>Cantidad: {unitsTaken}</p>
+            </div>
+            <div className="right">
+              {/* Botones para incrementar y reducir la cantidad de productos en el carrito */}
+              <button onClick={() => handleAdd(name)}>+</button>
+              <button onClick={() => handleMinus(name)}>-</button>
+            </div>
+          </section>
+        ))}
       </article>
+      {/* Botón de compra */}
       <input
         type="button"
         value="Purchase"
-        className={`purchase ${
-          Object.entries(userData?.cart).length !== 0 && "green"
-        }`}
-        disabled={Object.entries(userData?.cart).length === 0}
-        onClick={() => console.log(Object.entries(userData?.cart).length !== 0)}
+        className={`purchase ${Object.keys(userData?.cart).length !== 0 && "green"}`}
+        disabled={Object.keys(userData?.cart).length === 0}
+        onClick={() => console.log(Object.keys(userData?.cart).length !== 0)}
       />
     </aside>
   );
